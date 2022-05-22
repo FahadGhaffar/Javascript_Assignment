73153fe12a9f90d645e17c6387f8ff89a881fe80
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-05-22 15:15:58 -0700

    Assignement 2

---
+++ Asignment_2/app.js
@@ -0,0 +1,47 @@
+// Variable For Number 
+
+var age=12;
+alert(age);
+
+
+var visit=12;
+alert("You have visited this site "+visit+ " times");
+
+var birthYear=1998;
+document.write("Your birth year is "+birthYear+ " \n Data Type of my ddeclear variable is Number");
+
+var visiter_name="john";
+var Product="Apple";
+var Quality="Fruit"
+
+document.write(visiter_name + " ordered "+ Product+" on" +Quality);
+
+
+// Legal and Illegal 
+
+
+alert(visiter_name + " ordered "+ Product+" on" +Qualit);
+
+document.write("“Rules for naming JS variables”")
+
+
+var num1=3;
+var num2=5;
+var sum=num1+num2;
+
+
+document.write("Sum of "+num1+ " and  "+ num2 +" is "+sum);
+var temp ;
+document.write("Value after variable declearation is "+temp);
+
+temp=5;
+
+document.write("Initial value "+ temp);
+var newtemp=temp+ ++temp
+document.write("value after increment is "+temp);
+document.write("Value after "+newtemp);
+document.write("value after decreament is "--newtemp);
+
+document.write("The remaider is : 0")
+
+
