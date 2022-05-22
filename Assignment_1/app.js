8cef16c59c334b2572ea2c6474b6d0f32cbb9a59
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-05-22 12:45:08 -0700

    Assignemnet_1

--- Assignment_1/app.js
+++ Assignment_1/app.js
@@ -8,3 +8,30 @@ alert("Happy Coding!");
 alert("Hello... I Can Run Js through my web browser's console ");
 
 
+
+
+var username="Abc";
+var myName="Fahad Ghaffar";
+var message="Hello world";
+alert(message);
+
+var name="john";
+alert(name);
+var age="15";
+alert(age);
+var certifications="web and app";
+alert(certifications);
+
+alert("pizza \n pizz \n piz \n pi \n p");
+
+var email="example@gmail.com";
+alert("your email is "+email);
+
+var message="A smarter way to learn JavaScript."
+alert(message);
+
+document.write("Yah! I Can Write HTML Content Through JavaScript");
+
+
+alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+
