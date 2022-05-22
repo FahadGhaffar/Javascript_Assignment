aaaea1cc2231390df47e494868e676b2013e2cc6
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-05-22 09:48:54 -0700

    Assiginment1

--- Assignment_1/app.js
+++ Assignment_1/app.js
@@ -1,4 +1,10 @@
 
 
-alert();
+alert("Welcome to our site");
+alert("Error! Please enter a valid password");
+alert("Welcome to js land .. \n Happy Coding!");
+alert("Welcome to js land");
+alert("Happy Coding!");
+alert("Hello... I Can Run Js through my web browser's console ");
+
 
