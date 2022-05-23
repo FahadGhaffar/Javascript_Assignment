854316793bd15b98faaf24d0cd980f16c9973385
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-05-22 22:42:23 -0700

    class_assignement

---
+++ Class_Assignment/app.js
@@ -0,0 +1,19 @@
+
+
+
+var skill_html="yes";
+var skill_js="yes"
+var skill_css="yes";
+var age=18;
+var exp=2;
+var city="yes";
+
+
+if((skill_css === "yes" || skill_html === "yes") && skill_js==="yes" && age >= 18 && exp >=2 && city==="yes" ) {
+
+    console.log("welcome");
+
+}
+else{
+    console.log("try next time");
+}
