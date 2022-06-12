9453d206deaef9262cab37c939af42a59943d896
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-06-12 13:53:41 -0700

    Assignemnt 3

---
+++ Assignment_3/app.js
@@ -0,0 +1,61 @@
+
+
+var num = +prompt("Enter any number");
+
+
+document.write("Result :\n The value of a  is "+num +"\n") ;
+
+document.write("------------------------------------") ;
+document.write("The value of ++a  is "+(++num) +"\n") ;
+document.write("Now the value of a  is "+(num) +"\n") ;
+
+document.write("The value of a++  is "+(num++) +"\n") ;
+document.write("Now the value of a  is "+(num) +"\n") ;
+
+document.write("The value of --a  is "+(--num) +"\n") ;
+document.write("Now the value of a  is "+(num) +"\n") ;
+
+document.write("The value of a--  is "+(num--) +"\n") ;
+document.write("Now the value of a  is "+(num) +"\n") ;
+
+
+var a = 2, b = 1;
+var result = --a - --b + ++b + b--;
+
+document.writeln("Reuslt "+result);
+
+var name = prompt("Enter your name");
+document.writeln("Welcome "+name);
+
+
+var table =+ prompt("Enter table number",5);
+
+document.writeln(table+ "*1 ="+table*1);
+document.writeln(table+ "*2 ="+table*2);
+document.writeln(table+ "*3 ="+table*3);
+document.writeln(table+ "*4 ="+table*4);
+document.writeln(table+ "*5 ="+table*5);
+
+var Subject1 = prompt("Enter First subject");
+var Subject2 = prompt("Enter Second Subject");
+var subject3 = prompt("Enter Third Subject");
+
+var Subject1_score =+ prompt("Enter First Subject Score");
+var Subject2_score =+ prompt("Enter Second Subject Score");
+var Subject3_score =+ prompt("Enter Thired Subject Score");
+
+
+
+document.writeln("Subject Total Obtained Percentage");
+
+document.writeln(Subject1 +" 100 "+Subject1_score +" "+Subject1_score +" %");
+document.writeln(Subject2 +" 100 "+Subject2_score +" "+Subject2_score +" %");
+document.writeln(subject3 +" 100 "+Subject3_score +" "+Subject3_score +" %");
+
+
+
+
+
+
+
+
