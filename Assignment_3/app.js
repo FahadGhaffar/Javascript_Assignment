9589143c5e8c5ecc668d478e1a3862bd07a3a4cc
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-06-12 14:05:29 -0700

    changes

--- Assignment_3/app.js
+++ Assignment_3/app.js
@@ -22,19 +22,19 @@ document.write("Now the value of a  is "+(num) +"<br>") ;
 var a = 2, b = 1;
 var result = --a - --b + ++b + b--;
 
-document.writeln("Reuslt "+result);
+document.writeln("Reuslt "+result+"<br>");
 
-var name = prompt("Enter your name");
-document.writeln("Welcome "+name);
+var name = prompt("Enter your name ");
+document.writeln("Welcome "+name +"<br>");
 
 
 var table =+ prompt("Enter table number",5);
 
-document.writeln(table+ "*1 ="+table*1);
-document.writeln(table+ "*2 ="+table*2);
-document.writeln(table+ "*3 ="+table*3);
-document.writeln(table+ "*4 ="+table*4);
-document.writeln(table+ "*5 ="+table*5);
+document.writeln(table+ "*1 ="+table*1 +"<br>");
+document.writeln(table+ "*2 ="+table*2+"<br>");
+document.writeln(table+ "*3 ="+table*3+"<br>");
+document.writeln(table+ "*4 ="+table*4+"<br>");
+document.writeln(table+ "*5 ="+table*5+"<br>");
 
 var Subject1 = prompt("Enter First subject");
 var Subject2 = prompt("Enter Second Subject");
@@ -46,11 +46,11 @@ var Subject3_score =+ prompt("Enter Thired Subject Score");
 
 
 
-document.writeln("Subject Total Obtained Percentage");
+document.writeln("Subject Total Obtained Percentage"+"<br>");
 
-document.writeln(Subject1 +" 100 "+Subject1_score +" "+Subject1_score +" %");
-document.writeln(Subject2 +" 100 "+Subject2_score +" "+Subject2_score +" %");
-document.writeln(subject3 +" 100 "+Subject3_score +" "+Subject3_score +" %");
+document.writeln(Subject1 +" 100 "+Subject1_score +" "+Subject1_score +" %"+"<br>");
+document.writeln(Subject2 +" 100 "+Subject2_score +" "+Subject2_score +" %"+"<br>");
+document.writeln(subject3 +" 100 "+Subject3_score +" "+Subject3_score +" %"+"<br>");
 
 
 
