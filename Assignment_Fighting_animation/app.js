b72928ddc4d36193c681dc24e33c3fb1be108e3f
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-08-10 12:09:40 +0500

    Assignment

--- Assignment_Fighting_animation/app.js
+++ Assignment_Fighting_animation/app.js
@@ -1,78 +1,126 @@
-// var heading = document.querySelectorAll(".my-heading");
+// function generatePassword() {
+//   var user = 10;
+//   var randomStrings =
+//     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+";
+//   var randomPassword = "";
+//   var randomNumber = 0;
+//   for (var i = 0; i < user; i++) {
+//     randomNumber = Math.floor(Math.random() * randomStrings.length);
+//     randomPassword += randomStrings.charAt(randomNumber);
+//   }
+//   return randomPassword;
+// }
 
-// heading[0].innerHTML = "Ghous Ahmed"
-
-// var link = document.getElementById("link");
-
-// console.log(link.id);
-
-// var count = 1;
-
-// function scaleImage(zoom) {
-//   var image = document.getElementById("image");
-//   if (zoom === "+") {
-//     count++;
-//     image.style.transform = "scale(" + count + ")";
+// function emailValidation(para) {
+//   var email = document.getElementById(para);
+//   var password = document.getElementById("my-password");
+//   if (email.value) {
+//     console.log(email.value);
+//     email.style.backgroundColor = "white";
+//     password.value = generatePassword();
 //   } else {
-//     count--;
-//     image.style.transform = "scale(" + count + ")";
+//     email.style.backgroundColor = "red";
 //   }
 // }
 
-// var paragraph = document.getElementsByTagName("p");
-
-// console.log(paragraph)
-
-// var main = document.getElementById("main");
-
-// var paragraph = main.getElementsByTagName("p");
-
-// console.log(paragraph);
-
-// console.log(document.childNodes[1].childNodes[0].nextSibling.nodeName);
-
-// console.log(document.getElementById("main").setAttribute("class", "xyz"));
-
-// var main = document.getElementById("main");
-
-// var div = document.createElement("div");
-// var h1 = document.createElement("h1");
-// var text = document.createTextNode("Hello World");
-// h1.appendChild(text);
-// div.appendChild(h1);
-
-// main.appendChild(div);
-
-// function addTodo() {
-//   var inputValue = document.getElementById("todo-value");
-//   var list = document.getElementById("list");
-//   var li = document.createElement("li");
-//   var text = document.createTextNode(inputValue.value);
-//   var deleteBtn = document.createElement("button");
-//   deleteBtn.setAttribute("onclick", "deleteTodo()");
-//   var deleteText = document.createTextNode("Delete");
-//   deleteBtn.appendChild(deleteText);
-//   li.appendChild(text);
-//   li.appendChild(deleteBtn);
-//   list.appendChild(li);
-
-//   inputValue.value = "";
+// function submit() {
+//   if (event.keyCode === 13) {
+//     console.log("Login");
+//   }
 // }
 
-// function deleteAll() {
-//   var list = document.getElementById("list");
-//   list.innerHTML = "";
+// function getValue() {
+//   var heading = document.getElementById("heading");
+
+//   console.log(heading.innerHTML);
 // }
 
-// function deleteTodo() {
-//   event.target.parentNode.remove();
+// function setValue() {
+//   var heading = document.getElementById("heading");
+
+//   heading.innerText = "<span>Ghous Ahmed</span>";
 // }
 
-function foo() {
-  console.log("test");
-}
+// console.log("hello");
+// window.onload = function () {
+// };
 
-function abc() {
-  foo();
-  console.log(event.target);
-}
+// window.onkeydown = function () {
+//   console.log(event.keyCode);
+// };
+
+// window.onblur = function () {
+//   console.log("offline");
+// };
+
+// window.onfocus = function () {
+//   console.log("online");
+// };\
+
+// function getTime() {
+//   console.log(new Date().toTimeString());
+// }
+
+// setInterval(getTime, 1000);
+
+// setTimeout(function () {
+//   console.log("hello");
+// }, 2000);
+
+// console.log("one");
+// setTimeout(function () {
+//   console.log("two");
+// }, 1000);
+// console.log("three");
+
+// console.log(window);
+
+// console.log(document);
+// var count = 0;
+// var timer = setInterval(function () {
+//   count++;
+
+//   console.log(count);
+// }, 1000);
+
+// setTimeout(function () {
+//   clearInterval(timer);
+// }, 5000);
+
+// setTimeout(function () {
+//   timer = setInterval(function () {
+//     count++;
+
+//     console.log(count);
+//   }, 1000);
+// }, 7000);
+
+var count = 350;
+window.onkeydown = function () {
+  console.log(event.keyCode);
+  var character = document.getElementById("character");
+  if (event.keyCode === 39) {
+    character.src = "two.gif";
+    count = count + 50;
+    console.log(count);
+    character.style.left = count + "px";
+    setTimeout(function () {
+      character.src = "one.gif";
+    }, 100);
+  }
+  if (event.keyCode === 37) {
+    character.src = "two.gif";
+    count = count - 50;
+    console.log(count);
+    character.style.left = count + "px";
+    setTimeout(function () {
+      character.src = "one.gif";
+    }, 100);
+  }
+  if (event.keyCode === 65) {
+    character.src = "three.gif";
+    setTimeout(function () {
+      character.src = "one.gif";
+    }, 1000);
+  }
+};
