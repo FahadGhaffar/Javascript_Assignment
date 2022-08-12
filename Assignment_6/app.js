6958142bd7d36a268aee1fd3373641ae81ec03ac
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-08-12 14:15:15 +0500

    Assinment

--- Assignment_6/app.js
+++ Assignment_6/app.js
@@ -10,5 +10,15 @@ var user= prompt("Enter your food").toLowerCase();
 
 // console.log(shopMenu.indexOf(user))
 
-console.log(shopMenu[shopMenu.indexOf(user)]);
+if(shopMenu.indexOf(user)!== -1){
 
+    document.write("Yes We have available <h1>"+shopMenu[shopMenu.indexOf(user)]+"</h1>")
+
+}
+else{
+    document.write("Sorry We have not available your food but you can choice in between this menu <br/>")
+    for(var i=0 ; i<shopMenu.length ; i++){
+
+        document.write("<li>"+shopMenu[i]+"</li><br/>")
+    }
+}
