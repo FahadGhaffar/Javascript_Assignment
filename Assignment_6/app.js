ee72143f0ebcb590ad54949a34d8716edc9e2d0b
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-08-12 13:08:19 +0500

    Assignment

---
+++ Assignment_6/app.js
@@ -0,0 +1,14 @@
+ 
+
+
+var shopMenu=["biryani","nihari","qorma","pai"];
+
+
+
+var user= prompt("Enter your food").toLowerCase();
+
+
+// console.log(shopMenu.indexOf(user))
+
+console.log(shopMenu[shopMenu.indexOf(user)]);
+
