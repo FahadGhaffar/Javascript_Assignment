d4a6df4e24ad8b000e684af756d15a33ecded8e4
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-08-30 22:03:05 +0500

    assignment

--- Class_AssignmentTodo/app.js
+++ Class_AssignmentTodo/app.js
@@ -1,12 +1,33 @@
 
-
+var setitems=document.getElementById("listdata");
+var getdata=document.getElementById("getdata");
 function appendlist() {
-
-
-
-
-
-
-
-
+    setitems.innerHTML+=`<li> 
+    ${getdata.value} 
+    <button >Edit</button>
+     
+    </li> `
 }
+
+// const person = {
+//     name: 'Wes',
+//     job: 'Web Developer',
+//     city: 'Hamilton',
+//     bio: 'Wes is a really cool guy that loves to teach web development!'
+// };
+// // And then create our markup:
+// const markup = `
+//  <div class="person">
+//     <h2>
+//         ${person.name}
+//     </h2>
+//     <p class="location">${person.city}</p>
+//     <p class="bio">${person.bio}</p>
+//  </div>
+// `;
+
+
+// setitems.innerHTML = markup;
+
+// console.log("test");
+// alert("test")
