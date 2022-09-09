3de73c57efa44c9c6bb52786c1c5769f7acdb03a
Fahad Ghaffar <fahad.ghaffar97@gmail.com>
2022-09-08 23:58:46 -0700

    Assignment

--- Saylani_form/js/app.js
+++ Saylani_form/js/app.js
@@ -58,7 +58,8 @@ function set_on_focus() {
 }
 
 const userData = {
-    userStatus: false
+    userStatus: false,
+    message: ""
 
 };
 
@@ -66,20 +67,45 @@ function btnSubmit() {
 
 
 
-
+    console.log("ok")
     //  var name = document.forms["reg_form"].querySelectorAll('select', 'input')[1].value;
-    if (/^[0-9]{5}-[0-9]{7}-[0-9]$/.test(cinc)) {
+    if (/^[0-9]{5}-[0-9]{7}-[0-9]$/.test(cinc.value) && /^[0-9]{4}-[0-9]{7}$/.test(formphone.value) && /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(formemail.value)) {
         event.preventDefault();
 
-        document.forms["reg_form"].querySelectorAll('select').forEach((element) => {
+        document.forms["reg_form"].querySelectorAll('select, input').forEach((element) => {
+
             userData[element['id']] = element.value;
+            if (element['myatt'] === "true") {
+                console.log("okk");
+            }
         });
-        document.forms["reg_form"].querySelectorAll('input').forEach((element) => {
-            userData[element['id']] = element.value;
-        });
+
+        if (!fatherCnic.value && /^[0-9]{5}-[0-9]{7}-[0-9]$/.test(fatherCnic.value)) {
+            userData[fatherCnic['id']] = fatherCnic.value;
+        }
+        // document.forms["reg_form"].querySelectorAll('input').forEach((element) => {
+        //     userData[element['id']] = element.value;
+        // });
         console.log(userData)
         swal("Good job!", "You clicked the button!", "success");
 
+    } else {
+        swal({
+            title: "Are you sure?",
+            text: "Something gonna wrong",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        });
+        // .then((willDelete) => {
+        //     if (willDelete) {
+        //         swal("Poof! Your imaginary file has been deleted!", {
+        //             icon: "success",
+        //         });
+        //     } else {
+        //         swal("Your imaginary file is safe!");
+        //     }
+        // });
     }
 }
 // var name = document.forms["welcome_form"]["userName"].value;
